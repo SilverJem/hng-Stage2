dc813cbce473ad73f75629192339768c7cc18511
SilverJem <57413509+SilverJem@users.noreply.github.com>
2023-09-13 12:22:06 +0100

    Update Featured.jsx

--- src/Components/Featured.jsx
+++ src/Components/Featured.jsx
@@ -5,10 +5,10 @@ import Loader from './Loader';
 import { Link } from 'react-router-dom';
 
 const Featured = () => {
+
   const [movies, setMovies] = useState([]);
   const [imdbIds, setImdbIds] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
   const [heartClicked, setHeartClicked] = useState([]);
 
   const toggleHeartColor = (index) => {
@@ -30,6 +30,7 @@ const Featured = () => {
     axios(options)
       .then((response) => {
         setMovies(response.data.results.slice(0, 10));
+        console.log(response.data.results)
       })
       .catch((error) => {
         console.error(error);
@@ -45,15 +46,30 @@ const Featured = () => {
 
   useEffect(() => {
     const fetchImdbIds = async () => {
-      const options = {
-      url: 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1',
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MzMxNjg5NWNiMmQ1MzNiNWY4MWJmNjk2ZWNkYWQ1MSIsInN1YiI6IjY0ZmVjYmJhZGI0ZWQ2MTAzNDNlZjgwNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IRdbDejGYWwScmljrO1N1uRLGhQHBN0cRWvhVmQ6a64',
+      const ids = [];
+
+      for (const movie of movies) {
+        const url = `https://api.themoviedb.org/3/movie/${movie.id}/external_ids`;
+        const options = {
+          method: 'GET',
+          headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MzMxNjg5NWNiMmQ1MzNiNWY4MWJmNjk2ZWNkYWQ1MSIsInN1YiI6IjY0ZmVjYmJhZGI0ZWQ2MTAzNDNlZjgwNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IRdbDejGYWwScmljrO1N1uRLGhQHBN0cRWvhVmQ6a64',
+          },
+        };
+
+        try {
+          const response = await fetch(url, options);
+          const data = await response.json();
+          const imdbId = data.imdb_id;
+          ids.push(imdbId);
+        } catch (error) {
+          console.error('Error fetching IMDb ID:', error);
+        }
       }
-    }
-    }
+
+      setImdbIds(ids);
+    };
 
     if (movies.length > 0) {
       fetchImdbIds();
@@ -73,7 +89,8 @@ const Featured = () => {
       ) : (
         <div className="cards">
           {movies.map((item, index) => (
-            <div className="card container" key={item.id} data-testid="movie-card">
+            <div className="card" key={item.id} data-testid="movie-card">
+              <Link to={`/movies/${imdbIds[index]}`} style={{ textDecoration: 'none' }}>
               <div className="">
                 <img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt={item.original_title} data-testid="movie-poster" />
                 <div
@@ -82,6 +99,8 @@ const Featured = () => {
                   onClick={() => toggleHeartColor(index)}
                 ></div>
               </div>
+              
+              </Link>
               <div className="details">
                 <Link to={`/movies/${imdbIds[index]}`} style={{ textDecoration: 'none' }}>
                   <h4 data-testid="movie-title">{item.original_title}</h4>
