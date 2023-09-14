a7bdeab0e3e4212d2201607174f632e141665ff1
SilverJem <57413509+SilverJem@users.noreply.github.com>
2023-09-14 09:35:51 +0100

    Update Featured.jsx

--- src/Components/Featured.jsx
+++ src/Components/Featured.jsx
@@ -5,9 +5,7 @@ import Loader from './Loader';
 import { Link } from 'react-router-dom';
 
 const Featured = () => {
-
   const [movies, setMovies] = useState([]);
-  const [imdbIds, setImdbIds] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [heartClicked, setHeartClicked] = useState([]);
 
@@ -28,53 +26,20 @@ const Featured = () => {
     };
 
     axios(options)
-      .then((response) => {
-        setMovies(response.data.results.slice(0, 10));
-      
+      .then(async (response) => {
+        const moviesData = response.data.results.slice(0, 10);
+        setMovies(moviesData.map((movie, index) => ({ ...movie })));
+        setIsLoading(false);
       })
       .catch((error) => {
         console.error(error);
       });
+
   };
 
   useEffect(() => {
     getMovieData();
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
   }, []);
-
-  useEffect(() => {
-    const fetchImdbIds = async () => {
-      const ids = [];
-
-      for (const movie of movies) {
-        const url = `https://api.themoviedb.org/3/movie/${movie.id}/external_ids`;
-        const options = {
-          method: 'GET',
-          headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MzMxNjg5NWNiMmQ1MzNiNWY4MWJmNjk2ZWNkYWQ1MSIsInN1YiI6IjY0ZmVjYmJhZGI0ZWQ2MTAzNDNlZjgwNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IRdbDejGYWwScmljrO1N1uRLGhQHBN0cRWvhVmQ6a64',
-          },
-        };
-
-        try {
-          const response = await fetch(url, options);
-          const data = await response.json();
-          const imdbId = data.imdb_id;
-          ids.push(imdbId);
-        } catch (error) {
-          console.error('Error fetching IMDb ID:', error);
-        }
-      }
-
-      setImdbIds(ids);
-    };
-
-    if (movies.length > 0) {
-      fetchImdbIds();
-    }
-  }, [movies]);
 
   return (
     <section className="feature">
@@ -90,25 +55,23 @@ const Featured = () => {
         <div className="cards">
           {movies.map((item, index) => (
             <div className="card" key={item.id} data-testid="movie-card">
-              <Link to={`/movies/${imdbIds[index]}`} style={{ textDecoration: 'none' }}>
-              <div className="">
-                <img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt={item.original_title} data-testid="movie-poster" />
-                <div
-                  className="heart"
-                  style={{ backgroundColor: heartClicked[index] ? 'red' : 'white' }}
-                  onClick={() => toggleHeartColor(index)}
-                ></div>
-              </div>
-              
+              <Link to={`/movies/${item.id}`} style={{ textDecoration: 'none' }}>
+                <div className="">
+                  <img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt={item.original_title} data-testid="movie-poster" />
+                  <div
+                    className="heart"
+                    style={{ backgroundColor: heartClicked[index] ? 'red' : 'white' }}
+                    onClick={() => toggleHeartColor(index)}
+                  ></div>
+                </div>
               </Link>
               <div className="details">
-                <Link to={`/movies/${imdbIds[index]}`} style={{ textDecoration: 'none' }}>
+                <Link to={`/movies/${item.imdbId}`} style={{ textDecoration: 'none' }}>
                   <h4 data-testid="movie-title">{item.original_title}</h4>
                 </Link>
                 <div className="rate">
                   Released: <p data-testid="movie-release-date">{item.release_date}</p>
                 </div>
-               
               </div>
             </div>
           ))}
