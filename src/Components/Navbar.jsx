a9ad173e02193087a1691641853da4ff69973774
SilverJem <57413509+SilverJem@users.noreply.github.com>
2023-09-14 14:11:02 +0100

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,42 +2,54 @@ import React from 'react'
 import menu from './images/Menu.png'
 import find from './images/search.png'
 import logo from './images/tv.png'
-import  propTypes  from 'prop-types'
+import propTypes from 'prop-types'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-const Navbar = ({clas})=> {
-  const navigate = useNavigate()
-  const [content, setContent] = useState("")
+import Searched from './Searched'
+
+const Navbar = ({ clas }) => {
+  const [content, setContent] = useState('')
+  const [visible, setVisible] = useState(false) // Start with results hidden
+
+  const handleInputChange = (e) => {
+    const inputValue = e.target.value
+    setContent(inputValue)
+    // Show the results if the input has a value
+    setVisible(inputValue.trim() !== '')
+  }
 
   return (
     <div>
-       <nav className={clas}>
+      <nav className={clas}>
         <Link to="/">
-    <div className="logo">
-        <img src={logo}/>
-       <b>MovieBox</b> 
-    </div>
-    </Link>
-    <div className="search">
-      <input
-        type="text"
-        value={content}
-        placeholder="Enter something"
-        onChange={(e)=>setContent(e.target.value)}
-      />
-      <Link to={`/search/${content}`}>
-         <img src={find}/>  </Link>
-    </div>
-    <div className="login">
-        <a>Sign In</a>
-        <img src={menu}/>
-    </div>
-</nav> 
+          <div className="logo">
+            <img src={logo} alt="MovieBox Logo" />
+            <b>MovieBox</b>
+          </div>
+        </Link>
+        <div className="search">
+          <form>
+            <input
+              type="text"
+              value={content}
+              placeholder="Enter something"
+              onChange={handleInputChange}
+            />
+            <img src={find} alt="Search Icon" />
+          </form>
+          {visible ? <Searched con={content} /> : null}
+        </div>
+        <div className="login">
+          <a>Sign In</a>
+          <img src={menu} alt="Menu Icon" />
+        </div>
+      </nav>
     </div>
   )
 }
 
 export default Navbar
-Navbar.propTypes ={
-    clas:propTypes.string
+
+Navbar.propTypes = {
+  clas: propTypes.string
 }
